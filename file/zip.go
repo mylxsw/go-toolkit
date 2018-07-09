747bca61baff2ffef71b6a263acf3fb832a38675
管宜尧 <guanyiyao@yunsom.com>
2018-07-09 16:26:52 +0800

    add zip file create support

---
+++ file/zip.go
@@ -0,0 +1,76 @@
+package file
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+)
+
+// ZipFile is a file wrapper contains filename and path
+type ZipFile struct {
+	Name string
+	Path string
+}
+
+// CreateZipArchiveFile creaate a zip archive file from files
+func CreateZipArchiveFile(saveAs string, files []ZipFile) error {
+	saveAsFile, err := os.Create(saveAs)
+	if err != nil {
+		return fmt.Errorf("can not create destination zip archive file: %s", err.Error())
+	}
+
+	defer saveAsFile.Close()
+
+	return CreateZipArchive(saveAsFile, files)
+}
+
+// CreateZipArchive create a zip archive
+func CreateZipArchive(w io.Writer, files []ZipFile) error {
+	zipWriter := zip.NewWriter(w)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+		if err := addFileToZipArchive(zipWriter, file); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// addFileToZipArchive add a file to zip archive
+func addFileToZipArchive(zipWriter *zip.Writer, file ZipFile) error {
+
+	zipfile, err := os.Open(file.Path)
+	if err != nil {
+		return fmt.Errorf("can not open %s: %s", file.Path, err.Error())
+	}
+
+	defer zipfile.Close()
+
+	info, err := zipfile.Stat()
+	if err != nil {
+		return fmt.Errorf("can not get file state for %s: %s", file.Path, err.Error())
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return fmt.Errorf("create zip file header failed for %s: %s", file.Path, err.Error())
+	}
+
+	header.Name = file.Name
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return fmt.Errorf("create header failed for %s: %s", file.Path, err.Error())
+	}
+
+	_, err = io.Copy(writer, zipfile)
+	if err != nil {
+		return fmt.Errorf("copy file content failed for %s: %s", file.Path, err.Error())
+	}
+
+	return nil
+}
---
+++ file/zip_test.go
@@ -0,0 +1,23 @@
+package file
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCreateZipArchiveFile(t *testing.T) {
+
+	files := []ZipFile{
+		{Name: "file_test.go", Path: "file_test.go"},
+		{Name: "file.go", Path: "file.go"},
+		{Name: "zip.go", Path: "zip.go"},
+	}
+
+	zipfile := "xxx.zip"
+
+	if err := CreateZipArchiveFile(zipfile, files); err != nil {
+		t.Errorf("test failed: %s", err.Error())
+	}
+
+	os.Remove(zipfile)
+}
