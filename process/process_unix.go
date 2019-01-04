e56fc6fc5e70de51a1ca02e0cf41fc0230b4fdac
管宜尧 <guanyiyao@yunsom.com>
2019-01-04 15:04:09 +0800

    进程管理器不重新创建进程组，避免主进程意外挂掉后，子进程成为孤儿进程

--- process/process_unix.go
+++ process/process_unix.go
@@ -10,9 +10,10 @@ import (
 
 func (process *Process) createCmd() *exec.Cmd {
 	cmd := exec.Command(process.GetCommand(), process.GetArgs()...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true,
-	}
+	// 这里就不重新创建进程组了，创建新的进程组，在当前程序意外退出后，启动的外部进程时无法自动关闭的
+	// cmd.SysProcAttr = &syscall.SysProcAttr{
+	// 	Setpgid: true,
+	// }
 
 	if process.uid != "" {
 		cmd.SysProcAttr.Credential = createCredential(process.uid)
