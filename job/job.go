1ae07fa33f19a97e16e1e4f0f08f64076f8c63f1
管宜尧 <guanyiyao@yunsom.com>
2019-01-11 10:46:14 +0800

    add job process

---
+++ job/job.go
@@ -0,0 +1,71 @@
+package job
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/mylxsw/go-toolkit/container"
+	"github.com/mylxsw/go-toolkit/log"
+)
+
+var logger = log.Module("adanos.job")
+
+// Job is a interface for a job
+type Job interface {
+	Handle()
+}
+
+// Manager 周期性任务管理器
+type Manager struct {
+	container *container.Container
+	ctx       context.Context
+
+	wg sync.WaitGroup
+}
+
+// NewManager 创建一个Manager
+func NewManager(ctx context.Context, cc *container.Container) *Manager {
+	return &Manager{
+		container: cc,
+		ctx:       ctx,
+	}
+}
+
+// Run 启动周期性任务循环
+func (jm *Manager) Run(name string, job Job, interval time.Duration) {
+	logger.Debugf("Job %s 运行中...", name)
+	jm.wg.Add(1)
+
+	go func() {
+		globalTicker := time.NewTicker(interval)
+		defer func() {
+			globalTicker.Stop()
+			jm.wg.Done()
+		}()
+
+		for {
+			select {
+			case <-globalTicker.C:
+				func() {
+					defer func() {
+						if err := recover(); err != nil {
+							logger.Errorf("Job %s 发生异常：%s", name, err)
+						}
+					}()
+					if err := jm.container.Resolve(job.Handle); err != nil {
+						logger.Errorf("Job %s 执行失败: %s", name, err)
+					}
+				}()
+			case <-jm.ctx.Done():
+				logger.Debugf("Job %s 已停止", name)
+				return
+			}
+		}
+	}()
+}
+
+// Wait 等待所有任务结束
+func (jm *Manager) Wait() {
+	jm.wg.Wait()
+}
